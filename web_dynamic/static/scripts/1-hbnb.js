ee4f219027503a590cc114e28ea18c144b5eac55
edisson11osorio <osorioedisson08@gmail.com>
2022-06-06 10:07:56 -0500

    🚧 work in progress

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,13 @@
-// crear un script con vanilla 
+document.addEventListener('DOMContentLoaded', () => {
+    let checkboxes = document.querySelectorAll(".each_amenity");
+    let amenitiesChecked_list = [];
+
+    document.addEventListener("change", function() {
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.checked) {
+                amenitiesChecked_list += checkbox.getAttribute("data-name") + ", ";
+            }
+        });
+        document.getElementById("amenities_selected").innerText = amenitiesChecked_list;
+    });
+});
