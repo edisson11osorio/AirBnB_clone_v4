28d6a0e11df70d7f03b624e2adb814e1f65d5248
Migdalis <miglepage@gmail.com>
2022-06-09 04:15:55 -0500

    :white_check_mark: Add API status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,1 +1,24 @@
-// crear un script con vanilla 
+document.addEventListener('DOMContentLoaded', () => {
+    const checked_list = {};
+    const checkbox = document.querySelectorAll('input[type=checkbox]').forEach(function (checks) {
+        checks.addEventListener("change", function () {
+            if (this.checked) {
+                checked_list[checks.getAttribute('data-id')] = checks.getAttribute('data-name');
+            }
+            else {
+                delete checked_list[checks.getAttribute('data-id')];
+            }
+            document.querySelector(".amenities h4").textContent = Object.values(checked_list).join(', ');
+        });
+    });
+
+    const api_status = document.querySelector('div#api_status');
+    fetch("http://localhost:5001/api/v1/status/").then((response) => {
+        if (response.ok) {
+            api_status.className += " available";
+        }
+        else {
+            api_status.className.replace(" available", '');
+        }
+    });
+});
