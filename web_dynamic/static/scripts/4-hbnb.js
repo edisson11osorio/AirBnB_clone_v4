2d0cb202955d250fd1091335e1284a5999ea9d99
Migdalis <miglepage@gmail.com>
2022-06-12 19:36:27 -0500

    :sparkles: Add a function to load places initial or search

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -22,12 +22,71 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    fetch("http://localhost:5001/api/v1/places_search", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
+    .then(response => response.json())
+    .then(data => {
+        load_places(data)
+    });
+
+    function load_places(data) {
+        const section_place = document.querySelector(".places");
+        if (section_place.hasChildNodes()) {
+            while (section_place.firstChild) {
+                section_place.removeChild(section_place.firstChild);
+            }
+        }
+
+        for (let i = 0; i < data.length; i++) {
+            let article = document.createElement("article"),
+                title_box = document.createElement("div"),
+                title_name = document.createElement("h2"),
+                title_price = document.createElement("div"),
+                info = document.createElement("div"),
+                info_guest = document.createElement("div"),
+                info_rooms = document.createElement("div"),
+                info_bathrooms = document.createElement("div"),
+                description = document.createElement("div");
+
+            title_box.setAttribute('class', 'title_box');
+            title_name.innerHTML = data[i].name;
+            title_price.setAttribute('class', 'price_by_night');
+            title_price.innerHTML = '$' + data[i].price_by_night;
+            title_box.appendChild(title_name);
+            title_box.appendChild(title_price);
+
+            info.setAttribute('class', 'information');
+            info_guest.setAttribute('class', 'max_guest');
+            info_guest.innerHTML = data[i].max_guest + ' Guest' + (data[i].max_guest > 1 ? 's' : '');
+            info_rooms.setAttribute('class', 'number_rooms');
+            info_rooms.innerHTML = data[i].number_rooms + ' Bedroom' + (data[i].number_rooms > 1 ? 's' : '');
+            info_bathrooms.setAttribute('class', 'number_bathrooms');
+            info_bathrooms.innerHTML = data[i].number_bathrooms + ' Bathroom' + (data[i].number_bathrooms > 1 ? 's' : '');
+            info.appendChild(info_guest);
+            info.appendChild(info_rooms);
+            info.appendChild(info_bathrooms);
+
+            description.setAttribute('class', 'description');
+            description.innerHTML = data[i].description;
+
+            article.appendChild(title_box);
+            article.appendChild(info);
+            article.appendChild(description);
+
+            section_place.appendChild(article);
+        }
+    }
+
     const button = document.querySelector('[type=button]');
     button.addEventListener("click", function () {
         let request_data = {
             amenities: Object.keys(checked_list)
         }
-        console.log(request_data);
         fetch("http://localhost:5001/api/v1/places_search", {
             method: 'POST',
             headers: {
@@ -37,52 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(response => response.json())
         .then(data => {
-            const section_place = document.querySelector(".places");
-            console.log(data);
-            if (section_place.hasChildNodes()) {
-                while (section_place.firstChild) {
-                    section_place.removeChild(section_place.firstChild);
-                }
-            }
-
-            for (let i = 0; i < data.length; i++) {
-                let article = document.createElement("article"),
-                    title_box = document.createElement("div"),
-                    title_name = document.createElement("h2"),
-                    title_price = document.createElement("div"),
-                    info = document.createElement("div"),
-                    info_guest = document.createElement("div"),
-                    info_rooms = document.createElement("div"),
-                    info_bathrooms = document.createElement("div"),
-                    description = document.createElement("div");
-    
-                title_box.setAttribute('class', 'title_box');
-                title_name.innerHTML = data[i].name;
-                title_price.setAttribute('class', 'price_by_night');
-                title_price.innerHTML = '$' + data[i].price_by_night;
-                title_box.appendChild(title_name);
-                title_box.appendChild(title_price);
-    
-                info.setAttribute('class', 'information');
-                info_guest.setAttribute('class', 'max_guest');
-                info_guest.innerHTML = data[i].max_guest + ' Guest' + (data[i].max_guest > 1 ? 's' : '');
-                info_rooms.setAttribute('class', 'number_rooms');
-                info_rooms.innerHTML = data[i].number_rooms + ' Bedroom' + (data[i].number_rooms > 1 ? 's' : '');
-                info_bathrooms.setAttribute('class', 'number_bathrooms');
-                info_bathrooms.innerHTML = data[i].number_bathrooms + ' Bathroom' + (data[i].number_bathrooms > 1 ? 's' : '');
-                info.appendChild(info_guest);
-                info.appendChild(info_rooms);
-                info.appendChild(info_bathrooms);
-    
-                description.setAttribute('class', 'description');
-                description.innerHTML = data[i].description;
-    
-                article.appendChild(title_box);
-                article.appendChild(info);
-                article.appendChild(description);
-    
-                section_place.appendChild(article);
-            }
+            load_places(data);
         })
     })
 });
