8ad5cca120962e9fc6436f60397e7b174e0d155f
Migdalis <miglepage@gmail.com>
2022-06-12 19:16:03 -0500

    :white_check_mark: Fix indextation file 3-hbnb.js and add filters dynamic

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -31,44 +31,44 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(response => response.json())
     .then(data => {
-            const section_place = document.querySelector(".places")
-            for (let i = 0; i < data.length; i++) {
-                let article = document.createElement("article"),
-                    title_box = document.createElement("div"),
-                    title_name = document.createElement("h2"),
-                    title_price = document.createElement("div"),
-                    info = document.createElement("div"),
-                    info_guest = document.createElement("div"),
-                    info_rooms = document.createElement("div"),
-                    info_bathrooms = document.createElement("div"),
-                    description = document.createElement("div");
+        const section_place = document.querySelector(".places")
+        for (let i = 0; i < data.length; i++) {
+            let article = document.createElement("article"),
+                title_box = document.createElement("div"),
+                title_name = document.createElement("h2"),
+                title_price = document.createElement("div"),
+                info = document.createElement("div"),
+                info_guest = document.createElement("div"),
+                info_rooms = document.createElement("div"),
+                info_bathrooms = document.createElement("div"),
+                description = document.createElement("div");
 
-                title_box.setAttribute('class', 'title_box');
-                title_name.innerHTML = data[i].name;
-                title_price.setAttribute('class', 'price_by_night');
-                title_price.innerHTML = '$' + data[i].price_by_night;
-                title_box.appendChild(title_name);
-                title_box.appendChild(title_price);
-                
-                info.setAttribute('class', 'information');
-                info_guest.setAttribute('class', 'max_guest');
-                info_guest.innerHTML = data[i].max_guest + ' Guest' + (data[i].max_guest > 1 ? 's' : '');
-                info_rooms.setAttribute('class', 'number_rooms');
-                info_rooms.innerHTML = data[i].number_rooms + ' Bedroom' + (data[i].number_rooms > 1 ? 's' : '');
-                info_bathrooms.setAttribute('class', 'number_bathrooms');
-                info_bathrooms.innerHTML = data[i].number_bathrooms + ' Bathroom' + (data[i].number_bathrooms > 1 ? 's' : '');
-                info.appendChild(info_guest);
-                info.appendChild(info_rooms);
-                info.appendChild(info_bathrooms);
+            title_box.setAttribute('class', 'title_box');
+            title_name.innerHTML = data[i].name;
+            title_price.setAttribute('class', 'price_by_night');
+            title_price.innerHTML = '$' + data[i].price_by_night;
+            title_box.appendChild(title_name);
+            title_box.appendChild(title_price);
 
-                description.setAttribute('class', 'description');
-                description.innerHTML = data[i].description;
+            info.setAttribute('class', 'information');
+            info_guest.setAttribute('class', 'max_guest');
+            info_guest.innerHTML = data[i].max_guest + ' Guest' + (data[i].max_guest > 1 ? 's' : '');
+            info_rooms.setAttribute('class', 'number_rooms');
+            info_rooms.innerHTML = data[i].number_rooms + ' Bedroom' + (data[i].number_rooms > 1 ? 's' : '');
+            info_bathrooms.setAttribute('class', 'number_bathrooms');
+            info_bathrooms.innerHTML = data[i].number_bathrooms + ' Bathroom' + (data[i].number_bathrooms > 1 ? 's' : '');
+            info.appendChild(info_guest);
+            info.appendChild(info_rooms);
+            info.appendChild(info_bathrooms);
 
-                article.appendChild(title_box);
-                article.appendChild(info);
-                article.appendChild(description);
+            description.setAttribute('class', 'description');
+            description.innerHTML = data[i].description;
 
-                section_place.appendChild(article);
-            }
-        })
+            article.appendChild(title_box);
+            article.appendChild(info);
+            article.appendChild(description);
+
+            section_place.appendChild(article);
+        }
+    })
 });
